04e0308afb1381614d7bc81adca246a3d3fe3787
Jacob Storen <jjs@pvv.org>
2014-02-25 23:41:53 +0100

    Start of GuardedIndices implementation for MemReusing Vector

--- MemoryReusingVector.h
+++ MemoryReusingVector.h
@@ -6,22 +6,8 @@
 
 const size_t UNDEFINED_SIZE_T = (size_t)-1;
 
-#if 0
-// Some Idea ...
-template<Object>
-class IdxHandle
-{
+#if 1
 
-   // Needs static access to MemVector ...
-
-   Object* operator ->() { }
-   bool isNull();
-
-private:
-   size_t idx;
-   size_t reAllocCount;
-   template<Object> MemoryReusingVector* m_dataArray;
-}
 #endif
 
 
@@ -34,12 +20,28 @@ public:
 
    void     reserve(size_t size)       { m_elements.reserve(size); }
 
-   size_t   create();
-   void     erase(size_t idx);
+   class GuardedIndex
+   {
+      size_t index : 41;
+      size_t reAllocCount: 23;
+      friend typename MemoryReusingVector<Object>;
+   };
 
-   bool     isValid(size_t idx)        { return m_elements[idx].isValid; }
-   Object*  operator[] (size_t idx)    { return this->get(idx); }
-   Object*  get(size_t idx);
+   GuardedIndex   create();
+   void     erase(GuardedIndex idx);
+
+   bool     isValid(GuardedIndex idx)  { return m_elements[idx].isValid; }
+   Object*  operator[] (size_t idx)
+   {
+      if (!m_elements[idx].isValid)
+      {
+         return NULL;
+      }
+
+      return reinterpret_cast<Object*>(&(m_elements[idx].objectData));
+   }
+
+   Object*  get(GuardedIndex idx);
 
    size_t size()                       { return m_elements.size(); }
 
@@ -52,7 +54,8 @@ private:
           size_t idxToNextFreeElement;
       };
 
-      bool isValid;
+      unsigned int reAllocCount : 23;
+      bool isValid : 1;
    };
 
    size_t m_idxToNextFreeElement;
@@ -73,9 +76,9 @@ MemoryReusingVector<Object>::~MemoryReusingVector()
 }
 
 template <typename Object>
-size_t MemoryReusingVector<Object>::create()
+MemoryReusingVector<Object>::GuardedIndex MemoryReusingVector<Object>::create()
 {
-   size_t newIdx = UNDEFINED_SIZE_T;
+   GuardedIndex newIdx;
 
    if (m_idxToNextFreeElement == UNDEFINED_SIZE_T)
    {
@@ -87,7 +90,8 @@ size_t MemoryReusingVector<Object>::create()
 
    void * objMem = reinterpret_cast<void*>(&(m_elements[m_idxToNextFreeElement].objectData));
    m_elements[m_idxToNextFreeElement].isValid = true;
-   newIdx = m_idxToNextFreeElement;
+   newIdx.index = m_idxToNextFreeElement;
+   newIdx.reAllocCount = m_elements[m_idxToNextFreeElement].reAllocCount;
    m_idxToNextFreeElement = m_elements[m_idxToNextFreeElement].idxToNextFreeElement;
 
    // Use placement new to construct an object in the previously allocated memory
@@ -98,29 +102,32 @@ size_t MemoryReusingVector<Object>::create()
 }
 
 template <typename Object>
-void MemoryReusingVector<Object>::erase(size_t idx)
+void MemoryReusingVector<Object>::erase(MemoryReusingVector<Object>::GuardedIndex idx)
 {
-   if (!m_elements[idx].isValid)
+   if (!m_elements[idx.index].isValid || m_elements[idx.index].reAllocCount != idx.reAllocCount)
    {
       return;
    }
+
    Object* objToDelete = reinterpret_cast<Object*>(&(m_elements[idx].objectData));
    objToDelete->~Object();
 
-   m_elements[idx].isValid = false;
-   m_elements[idx].idxToNextFreeElement = m_idxToNextFreeElement;
-   m_idxToNextFreeElement = idx;
+   m_elements[idx.index].isValid = false;
+   m_elements[idx.index].idxToNextFreeElement = m_idxToNextFreeElement;
+   ++(m_elements[idx.index].reAllocCount);
+
+   m_idxToNextFreeElement = idx.index;
 }
 
 template <typename Object>
-Object*  MemoryReusingVector<Object>::get(size_t idx)
+Object*  MemoryReusingVector<Object>::get(GuardedIndex idx)
 {
-   if (!m_elements[idx].isValid)
+   if (!m_elements[idx].isValid || m_elements[idx.index].reAllocCount != idx.reAllocCount)
    {
       return NULL;
    }
 
-   return reinterpret_cast<Object*>(&(m_elements[idx].objectData));
+   return reinterpret_cast<Object*>(&(m_elements[idx.index].objectData));
 }
 
 #endif // MEMORYREUSINGVECTOR_H
