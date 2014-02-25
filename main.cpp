1aa6e3da6f5c303b9565738405b246e2930b718c
Jacob Storen <jjs@pvv.org>
2014-02-25 19:51:42 +0100

    Start of MemReusingVector

--- main.cpp
+++ main.cpp
@@ -4,6 +4,78 @@
 
 using namespace std;
 
+const size_t UNDEFINED_SIZE_T = (size_t)-1;
+
+#if 0
+template<Object>
+class IdxHandle
+{
+
+   // Needs static access to MemVector ...
+
+   Object* operator ->() { }
+   bool isNull();
+
+private:
+   size_t idx;
+   size_t reAllocCount;
+   template<Object> MemoryReusingVector* m_dataArray;
+}
+#endif
+#if 0
+template < typename Object>
+class MemoryReusingVector
+{
+   MemoryReusingVector() : nextFreeElement(UNDEFINED_SIZE_T) {}
+   void reserve(size_t size)
+   {
+
+   }
+
+   size_t create()
+   {
+      size_t newIdx = UNDEFINED_SIZE_T;
+
+      if (nextFreeElement == UNDEFINED_SIZE_T)
+      {
+         m_elements.push_back(Element());
+         nextFreeElement = m_elements.size()-1;
+      }
+
+      // Use placement new to construct an object in the previously allocated memory
+
+      void * objMem = reinterpret_cast<void*>(&(m_elements[m_nextFreeElement].object));
+      new (objMem) Object;
+
+      m_elements[m_nextFreeElement].isValid = true;
+      if ()
+      m_nextFreeElement = m_nextFreeElement->objectMemory.nextFreeElement;
+
+
+
+   }
+   bool isValid(size_t idx);
+   Object& get(size_t idx);
+   void erase(size_t idx);
+
+private:
+   class Element
+   {
+      union
+      {
+          char object[sizeof(Object)];
+          size_t nextFreeElement;
+      };
+
+      bool isValid;
+   };
+
+   size_t nextFreeElement;
+
+   std::vector<Element> m_elements;
+};
+  #endif
+
 class A
 {
 public:
